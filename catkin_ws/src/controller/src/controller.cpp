9f329296a3f01c3996d223b83eb7780b32a8fab8
John Blankenhorn <jrblankenhorn@wpi.edu>
2019-04-25 16:49:59 -0400

    updated a few things

--- catkin_ws/src/controller/src/controller.cpp
+++ catkin_ws/src/controller/src/controller.cpp
@@ -276,60 +276,61 @@ const TurtlebotState Controller::getDesiredState()
 
 const TurtlebotState Controller::integrateDesiredStateToCurrentTime(const int &traj_it, const double &dt) const
 {
-    double acc_ang = 0;
-    acc_ang = (m_traj->yaw_rates[traj_it] - m_traj->yaw_rates[traj_it - 1]) / m_traj->durations[traj_it];
-    if(std::isnan(acc_ang) || std::isinf(acc_ang))
-    {
-        acc_ang = 0;
-    }
-    const double &th_dot = m_traj->yaw_rates[traj_it - 1] + acc_ang * dt;
-    const double &th = m_traj->headings[traj_it - 1] + th_dot * dt + acc_ang * std::pow(dt, 2) / 2;
-    double jerk_x = 0;
-    double jerk_y = 0;
-    double acc_x = 0;
-    double acc_y = 0;
-    if(traj_it < m_traj->durations.size() - 4)
-    {
-        const double &xp3 = m_traj->x_values[traj_it + 3];
-        const double &xp2 = m_traj->x_values[traj_it + 2];
-        const double &xp1 = m_traj->x_values[traj_it + 1];
-        const double &xp0 = m_traj->x_values[traj_it];
-        const double &vxp2 = (xp3 - xp2) / m_traj->durations[traj_it];
-        const double &vxp1 = (xp2 - xp1) / m_traj->durations[traj_it];
-        const double &vxp0 = (xp1 - xp0) / m_traj->durations[traj_it];
-        const double &accxp1 = (vxp2 - vxp1) / m_traj->durations[traj_it];
-        acc_x = (vxp1 - vxp0) / m_traj->durations[traj_it];
-        jerk_x = (accxp1 - acc_x) / m_traj->durations[traj_it];
-        const double &yp3 = m_traj->y_values[traj_it + 3];
-        const double &yp2 = m_traj->y_values[traj_it + 2];
-        const double &yp1 = m_traj->y_values[traj_it + 1];
-        const double &yp0 = m_traj->y_values[traj_it];
-        const double &vyp2 = (yp3 - yp2) / m_traj->durations[traj_it];
-        const double &vyp1 = (yp2 - yp1) / m_traj->durations[traj_it];
-        const double &vyp0 = (yp1 - yp0) / m_traj->durations[traj_it];
-        const double &accyp1 = (vyp2 - vyp1) / m_traj->durations[traj_it];
-        acc_y = (vyp1 - vyp0) / m_traj->durations[traj_it];
-        jerk_y = (accyp1 - acc_y) / m_traj->durations[traj_it];
-    }
-    const double &v = m_traj->speeds[traj_it];
-    const double &v_x = v * cos(th);
-    const double &v_y = v * sin(th);
-    const double &x = m_traj->x_values[traj_it - 1] + v_x + acc_x * std::pow(dt, 2) / 2 + jerk_x * std::pow(dt, 3) / 6;
-    const double &y = m_traj->y_values[traj_it - 1] + v_y + acc_y * std::pow(dt, 2) / 2 + jerk_y * std::pow(dt, 3) / 6;
-    return TurtlebotState(x, y, th, v, th_dot, v_x, v_y, acc_x, acc_y, jerk_x, jerk_y);
+  double acc_ang = 0;
+      acc_ang = (m_traj->yaw_rates[traj_it] - m_traj->yaw_rates[traj_it - 1]) / m_traj->durations[traj_it];
+      if(std::isnan(acc_ang) || std::isinf(acc_ang))
+      {
+          acc_ang = 0;
+      }
+      const double &th_dot = m_traj->yaw_rates[traj_it - 1] + acc_ang * dt;
+      const double &th = m_traj->headings[traj_it - 1] + th_dot * dt + acc_ang * std::pow(dt, 2) / 2;
+      double jerk_x = 0;
+      double jerk_y = 0;
+      double acc_x = 0;
+      double acc_y = 0;
+      if(traj_it < m_traj->durations.size() - 4)
+      {
+          const double &xp3 = m_traj->x_values[traj_it + 3];
+          const double &xp2 = m_traj->x_values[traj_it + 2];
+          const double &xp1 = m_traj->x_values[traj_it + 1];
+          const double &xp0 = m_traj->x_values[traj_it];
+          const double &vxp2 = (xp3 - xp2) / m_traj->durations[traj_it];
+          const double &vxp1 = (xp2 - xp1) / m_traj->durations[traj_it];
+          const double &vxp0 = (xp1 - xp0) / m_traj->durations[traj_it];
+          const double &accxp1 = (vxp2 - vxp1) / m_traj->durations[traj_it];
+          acc_x = (vxp1 - vxp0) / m_traj->durations[traj_it];
+          jerk_x = (accxp1 - acc_x) / m_traj->durations[traj_it];
+          const double &yp3 = m_traj->y_values[traj_it + 3];
+          const double &yp2 = m_traj->y_values[traj_it + 2];
+          const double &yp1 = m_traj->y_values[traj_it + 1];
+          const double &yp0 = m_traj->y_values[traj_it];
+          const double &vyp2 = (yp3 - yp2) / m_traj->durations[traj_it];
+          const double &vyp1 = (yp2 - yp1) / m_traj->durations[traj_it];
+          const double &vyp0 = (yp1 - yp0) / m_traj->durations[traj_it];
+          const double &accyp1 = (vyp2 - vyp1) / m_traj->durations[traj_it];
+          acc_y = (vyp1 - vyp0) / m_traj->durations[traj_it];
+          jerk_y = (accyp1 - acc_y) / m_traj->durations[traj_it];
+      }
+      const double &acc_lin = std::sqrt(std::pow(acc_x, 2) + std::pow(acc_y, 2));
+      const double &v = m_traj->speeds[traj_it - 1] + acc_lin * dt;
+      const double &v_x = v * cos(th);
+      const double &v_y = v * sin(th);
+      const double &x = m_traj->x_values[traj_it - 1] + v_x * dt + acc_x * std::pow(dt, 2) / 2 + jerk_x * std::pow(dt, 3) / 6;
+      const double &y = m_traj->y_values[traj_it - 1] + v_y * dt + acc_y * std::pow(dt, 2) / 2 + jerk_y * std::pow(dt, 3) / 6;
+      return TurtlebotState(x, y, th, v, th_dot, v_x, v_y, acc_x, acc_y, jerk_x, jerk_y);
 }
 
 void Controller::integratePoseToCurrentTime()
 {
     ros::Duration dt = ros::Time::now() - m_odom->header.stamp;
-    const double &acc_x = (m_odom->twist.twist.linear.x - m_prev_odom->twist.twist.linear.x) / dt.toSec();
-    const double &acc_y = (m_odom->twist.twist.linear.y - m_prev_odom->twist.twist.linear.y) / dt.toSec();
-    const double &acc_ang = (m_odom->twist.twist.angular.z - m_prev_odom->twist.twist.angular.z) / dt.toSec();
     tf::Quaternion q_p;
     tf::quaternionMsgToTF(m_pose->pose.pose.orientation, q_p);
     const double &yaw = tf::getYaw(q_p);
-    const double &v_x = m_odom->twist.twist.linear.x + acc_x * dt.toSec();
-    const double &v_y = m_odom->twist.twist.linear.y + acc_y * dt.toSec();
+    const double &acc_x = (m_odom->twist.twist.linear.x - m_prev_odom->twist.twist.linear.x) * cos(yaw) / dt.toSec();
+    const double &acc_y = (m_odom->twist.twist.linear.x - m_prev_odom->twist.twist.linear.x) * sin(yaw) / dt.toSec();
+    const double &acc_ang = (m_odom->twist.twist.angular.z - m_prev_odom->twist.twist.angular.z) / dt.toSec();
+    const double &v_x = m_odom->twist.twist.linear.x * cos(yaw) + acc_x * dt.toSec();
+    const double &v_y = m_odom->twist.twist.linear.x * sin(yaw) + acc_y * dt.toSec();
     const double &v_ang = m_odom->twist.twist.angular.z + acc_ang * dt.toSec();
     const double &yaw_f = yaw + m_odom->twist.twist.linear.z * dt.toSec() + acc_ang * std::pow(dt.toSec(), 2) / 2;
     double r = std::sqrt(std::pow(v_x, 2) + std::pow(v_y, 2)) / v_ang;
